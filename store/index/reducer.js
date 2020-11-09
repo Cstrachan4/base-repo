d9109445917ba3695ef2479a7e6732fbdf2f8858
Callum Strachan <callum.strachan@appliedworks.co.uk>
2020-11-09 11:16:55 +0000

    Add map deep get, fix styles link and simplify base reducer

--- store/index/reducer.js
+++ store/index/reducer.js
@@ -1,29 +1,17 @@
 import * as actionTypes from './actionTypes.js';
 
 const INITIAL_STATE = {
-  hovered: false
+  initial: true
 };
 
 const reducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
 
     case actionTypes.UPDATE_INDEX:
-      if (action.key === 'type' && action.value !== state.type && action.columns) {
-        // Attempt to preserve column selections when changing chart type, but
-        // reset everything else
-        return {
-          ...INITIAL_STATE,
-          type: action.value,
-          x: filterValidColumns(state.x, action.value, 'x', action.columns),
-          y: filterValidColumns(state.y, action.value, 'y', action.columns),
-        };
-      } else {
-        // Otherwise just update the specific key supplied
-        return {
-          ...state,
-          [action.key]: action.value,
-        };
-      }
+      return {
+        ...state,
+        [action.key]: action.value,
+      };
       break;
 
     default:
